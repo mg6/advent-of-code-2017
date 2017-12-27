c677cb7d29c5a3dcb97049786de2f8f55487d47c
Maciej Gamrat <maciej@gamrat.it>
2017-12-28 00:13:31 +0100

    Day 17: Use deque for great speedup, update tests

--- day17-spinlock/run.py
+++ day17-spinlock/run.py
@@ -1,67 +1,59 @@
 #!/usr/bin/env python3
 
 
+from collections import deque
 from itertools import count, islice
 
 
 def spinlock(step):
-    state = [0]
-    at = 0
-
+    state = deque([0])
     for n in count(1):
-        yield state, at
-        at = (at + step) % n
-        at += 1
-        state.insert(at, n)
+        yield state
+        state.rotate(-step)
+        state.append(n)
 
 
 s = spinlock(3)
 
-state, at = next(s)
-assert list(state) == [0]
-assert at == 0
+state = next(s)
+assert list(state) == [0], state
 
-state, at = next(s)
+state = next(s)
 assert list(state) == [0, 1], state
-assert at == 1
 
-state, at = next(s)
-assert list(state) == [0, 2, 1], state
-assert at == 1
+state = next(s)
+assert list(state) == [1, 0, 2], state
 
-state, at = next(s)
-assert list(state) == [0, 2, 3, 1], state
-assert at == 2
+state = next(s)
+assert list(state) == [1, 0, 2, 3], state
 
-state, at = next(s)
-assert list(state) == [0, 2, 4, 3, 1], state
-assert at == 2
+state = next(s)
+assert list(state) == [3, 1, 0, 2, 4], state
 
-state, at = next(s)
-assert list(state) == [0, 5, 2, 4, 3, 1], state
-assert at == 1
+state = next(s)
+assert list(state) == [2, 4, 3, 1, 0, 5], state
 
-state, at = next(s)
-assert list(state) == [0, 5, 2, 4, 3, 6, 1], state
-assert at == 5
+state = next(s)
+assert list(state) == [1, 0, 5, 2, 4, 3, 6], state
 
-state, at = next(s)
-assert list(state) == [0, 5, 7, 2, 4, 3, 6, 1], state
-assert at == 2
+state = next(s)
+assert list(state) == [2, 4, 3, 6, 1, 0, 5, 7], state
 
-state, at = next(s)
-assert list(state) == [0, 5, 7, 2, 4, 3, 8, 6, 1], state
-assert at == 6
+state = next(s)
+assert list(state) == [6, 1, 0, 5, 7, 2, 4, 3, 8], state
 
-state, at = next(s)
-assert list(state) == [0, 9, 5, 7, 2, 4, 3, 8, 6, 1], state
-assert at == 1
+state = next(s)
+assert list(state) == [5, 7, 2, 4, 3, 8, 6, 1, 0, 9], state
 
 s = spinlock(3)
-state, at = next(islice(s, 2017, 2018))
-assert state[at + 1] == 638
+state = next(islice(s, 2017, 2018))
+assert state[0] == 638
 
 puzzle_input = 376
 s = spinlock(puzzle_input)
-state, at = next(islice(s, 2017, 2018))
-print(state[at + 1])
+state = next(islice(s, 2017, 2018))
+print(state[0])
+
+s = spinlock(puzzle_input)
+state = next(islice(s, 50000000, 50000000+1))
+print(state[state.index(0) + 1])
